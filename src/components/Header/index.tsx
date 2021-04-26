c6130044b60d56993909c08a8690d6b578b7af55
Thomas Soares <thomas__soares@hotmail.com>
2021-04-25 22:07:59 -0300

    Trabalhando com datas utilizando a biblioteca date-fns.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,12 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import styles from './styles.module.scss';
 
 export function Header() {
-  const currentDate = new Date().toLocaleDateString();
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+   locale: ptBR, 
+  });
 
   return (
     <header className={styles.headerContainer}>
