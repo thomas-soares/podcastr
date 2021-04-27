5058f35299aec60103f947dcf2aa3cfa2bf8c630
Thomas Soares <thomas__soares@hotmail.com>
2021-04-26 23:17:52 -0300

    Marcaçao do componente de Player.

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -3,6 +3,17 @@ import styles from './styles.module.scss';
 export function Player() {
 
   return (
-    <div className={styles.playerConatiner}></div>
+    <div className={styles.playerContainer}>
+      <header>
+        <img src="/playing.svg" alt="Tocando agora"/>
+        <strong>Tocando agora</strong>
+      </header>
+
+      <div className={styles.emptyPlayer}>
+        <strong>Selecione um podcast para ouvir</strong>
+      </div>
+
+      <footer></footer>
+    </div>
   );
 }
