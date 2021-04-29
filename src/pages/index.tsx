78384db3d257397546702103b5624c9602142553
Thomas Soares <thomas__soares@hotmail.com>
2021-04-28 23:07:24 -0300

    Declarando os tipos das props da home.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
-export default function Home(props) {
+import { GetStaticProps } from "next";
+
+type Episodes = {
+  id: string;
+  title: string;
+  members: string;
+}
+
+type HomeProps = {
+  episodes: Episodes[];
+}
+
+export default function Home(props: HomeProps) {
   return (
     <h1>Index</h1>
   );
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await fetch('http://localhost:3333/episodes');
   const data = await response.json();
 
