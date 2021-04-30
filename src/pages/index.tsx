14ed26803ab57f3a13f6bf6ae307a19230ffeeca
Thomas Soares <thomas__soares@hotmail.com>
2021-04-29 22:16:03 -0300

    Mapeando os dados que vem da api.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from "next";
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { api } from "../services/api";
 
 type Episodes = {
@@ -24,6 +26,19 @@ export const getStaticProps: GetStaticProps = async () => {
       _sort: 'published_at',
       _order: 'desc'
     }
+  });
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      description: episode.description,
+      url: episode.file.url,
+    };
   });
 
   return {
