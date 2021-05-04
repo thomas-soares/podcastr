029dd3516f81ee293041f7dafc0961ddc9e3467f
Thomas Soares <thomas__soares@hotmail.com>
2021-05-03 23:05:44 -0300

    Criando a geraçao estatica da pagina.

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,4 +1,9 @@
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { GetStaticProps } from 'next';
 import { useRouter } from 'next/router';
+import { api } from '../../services/api';
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
 export default function Episode() {
   const router = useRouter();
@@ -6,4 +11,27 @@ export default function Episode() {
   return (
     <h1>{router.query.slug}</h1>
   );
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { slug } = ctx.params;
+
+  const { data } = await api.get(`/episodes/${slug}`);
+
+  const episode = {
+    id: data.id,
+    title: data.title,
+    thumbnail: data.thumbnail,
+    members: data.members,
+    publishedAt: format(parseISO(data.published_at), 'd MMM yy', { locale: ptBR }),
+    duration: Number(data.file.duration),
+    durationAsString: convertDurationToTimeString(Number(data.file.duration)),
+    description: data.description,
+    url: data.file.url,
+  };
+
+  return {
+    episode,
+    revalidate: 60 * 60 * 24, // 24 hours
+  }
 }
