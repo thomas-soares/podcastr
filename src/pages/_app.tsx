7da91d0d39d187c07da128034812d8776df717bf
Thomas Soares <thomas__soares@hotmail.com>
2021-05-06 22:45:25 -0300

    Criando os estados do Player context.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
-import '../styles/global.scss'
+import { useState } from 'react';
 
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
 import { PlayerContext } from '../contexts/PlayerContext';
 
+import '../styles/global.scss'
 import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+
+
   return (
-    <PlayerContext.Provider value={'Thomas'}>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex }}>
       <div className={styles.wrapper}>
         <main>
           <Header />
