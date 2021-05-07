73b4e880bc57f36ae351517f4fed39034a6f9506
Thomas Soares <thomas__soares@hotmail.com>
2021-05-06 22:39:35 -0300

    Criando os tipos do PlayerContext.

--- src/contexts/PlayerContext.ts
+++ src/contexts/PlayerContext.ts
@@ -1,3 +1,16 @@
 import { createContext } from 'react';
 
-export const PlayerContext = createContext('');
+type Episode = {
+  title: string;
+  members: string;
+  thumbnail: string;
+  duration: string;
+  url: string;
+};
+
+type PlayerContextData = {
+  episodeList: Episode[];
+  currentEpisodeIndex: number;
+};
+
+export const PlayerContext = createContext({} as PlayerContextData);
