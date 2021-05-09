44583033fb87c8cbce68215fb85d54b9329753ab
Thomas Soares <thomas__soares@hotmail.com>
2021-05-08 23:24:20 -0300

    Utilizando funçao de play e pause.

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -9,7 +9,12 @@ import { PlayerContext } from '../../contexts/PlayerContext';
 import styles from './styles.module.scss';
 
 export function Player() {
-  const { episodeList, currentEpisodeIndex, isPlaying } = useContext(PlayerContext);
+  const {
+    episodeList,
+    currentEpisodeIndex,
+    isPlaying,
+    togglePlay
+  } = useContext(PlayerContext);
 
   const episode = episodeList[currentEpisodeIndex];
 
@@ -68,7 +73,12 @@ export function Player() {
           <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt="Tocar anterior" />
           </button>
-          <button type="button" className={styles.playButton} disabled={!episode}>
+          <button
+            type="button"
+            className={styles.playButton}
+            disabled={!episode}
+            onClick={togglePlay}
+          >
             { isPlaying
               ? <img src="/pause.svg" alt="Tocar" />
               : <img src="/play.svg" alt="Tocar" />
