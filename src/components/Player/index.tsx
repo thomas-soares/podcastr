af8e154b522294dc5f89febd7b38973fc5af6369
Thomas Soares <thomas__soares@hotmail.com>
2021-05-08 22:57:06 -0300

    Utilizando novo estado para fazer verificaçao se o episodio esta tocando.

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -9,7 +9,7 @@ import { PlayerContext } from '../../contexts/PlayerContext';
 import styles from './styles.module.scss';
 
 export function Player() {
-  const { episodeList, currentEpisodeIndex } = useContext(PlayerContext);
+  const { episodeList, currentEpisodeIndex, isPlaying } = useContext(PlayerContext);
 
   const episode = episodeList[currentEpisodeIndex];
 
@@ -69,7 +69,10 @@ export function Player() {
             <img src="/play-previous.svg" alt="Tocar anterior" />
           </button>
           <button type="button" className={styles.playButton} disabled={!episode}>
-            <img src="/play.svg" alt="Tocar" />
+            { isPlaying
+              ? <img src="/pause.svg" alt="Tocar" />
+              : <img src="/play.svg" alt="Tocar" />
+            }
           </button>
           <button type="button" disabled={!episode}>
             <img src="/play-next.svg" alt="Tocar próxima" />
