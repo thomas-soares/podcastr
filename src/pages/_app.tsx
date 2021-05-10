d91f692501176a491e2f0cba7b1035adca99a8ae
Thomas Soares <thomas__soares@hotmail.com>
2021-05-09 22:56:21 -0300

    Criando funçao para mudar estado do audio.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,8 +22,12 @@ function MyApp({ Component, pageProps }) {
     setIsPlaying(!isPlaying);
   }
 
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
   return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, isPlaying, play, togglePlay }}>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, isPlaying, play, togglePlay, setPlayingState }}>
       <div className={styles.wrapper}>
         <main>
           <Header />
