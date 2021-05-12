93ae29530ef900f0364bf344fe9f75c5b1092329
Thomas Soares <thomas__soares@hotmail.com>
2021-05-11 23:23:53 -0300

    Dando play no episodio na pagina de detalhe.

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -27,7 +27,7 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
-  const { } = usePlayer();
+  const { play } = usePlayer();
 
   return (
     <div className={styles.episode}>
@@ -43,7 +43,7 @@ export default function Episode({ episode }: EpisodeProps) {
           src={episode.thumbnail}
           objectFit="cover"
         />
-        <button type="button">
+        <button type="button" onClick={() => play(episode)}>
           <img src="/play.svg" alt="Tocar episódio" />
         </button>
       </div>
