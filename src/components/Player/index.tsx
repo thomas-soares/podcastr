fdae7d86abe6f8dfdb08b2e4a71207b6f158d7f1
Thomas Soares <thomas__soares@hotmail.com>
2021-05-11 23:12:54 -0300

    Usando booleanos passados pelo contexto para verificar se tem episodio proximo ou anterior.

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -17,6 +17,8 @@ export function Player() {
     isPlaying,
     togglePlay,
     setPlayingState,
+    hasNext,
+    hasPrevious,
     playNext,
     playPrevious
   } = useContext(PlayerContext);
@@ -91,7 +93,7 @@ export function Player() {
           <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Embaralhar" />
           </button>
-          <button type="button" onClick={playPrevious} disabled={!episode}>
+          <button type="button" onClick={playPrevious} disabled={!episode || hasPrevious}>
             <img src="/play-previous.svg" alt="Tocar anterior" />
           </button>
           <button
@@ -105,7 +107,7 @@ export function Player() {
               : <img src="/play.svg" alt="Tocar" />
             }
           </button>
-          <button type="button" onClick={playNext} disabled={!episode}>
+          <button type="button" onClick={playNext} disabled={!episode || hasNext}>
             <img src="/play-next.svg" alt="Tocar próxima" />
           </button>
           <button type="button" disabled={!episode}>
