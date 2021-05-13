ea827f21462f49b6569efbd77a73762a3cef0f01
Thomas Soares <thomas__soares@hotmail.com>
2021-05-12 23:29:55 -0300

    Utilizando estado e metodo de shuffle.

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -16,8 +16,10 @@ export function Player() {
     currentEpisodeIndex,
     isPlaying,
     isLooping,
+    isShuffling,
     togglePlay,
     toggleLoop,
+    toggleShuffle,
     setPlayingState,
     hasNext,
     hasPrevious,
@@ -93,7 +95,12 @@ export function Player() {
         ) }
 
         <div className={styles.buttons}>
-          <button type="button" disabled={!episode}>
+          <button
+            type="button"
+            onClick={toggleShuffle}
+            className={isShuffling ? styles.IsActive : ''}
+            disabled={!episode}
+          >
             <img src="/shuffle.svg" alt="Embaralhar" />
           </button>
           <button type="button" onClick={playPrevious} disabled={!episode || hasPrevious}>
